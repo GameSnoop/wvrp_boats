d5d6d05cd1bb25a68667da83a6f46c69511a84ba
GameSnoop <59174281+GameSnoop@users.noreply.github.com>
2019-12-24 19:49:57 -0500

    Add files via upload

---
+++ wvrp_boats/config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.NoMoney = 'Do not have enough money $'
+Config.Shoptext = 'Press (E) to Rent Boats'
+Config.LevelMissing = 'You do not have enough expirience' 
+Config.Coords = {
+	vector3(-723.45,-1278.27,42.88)
+}
+
+
+  
